5b3f347cb95f8ded9d88d2079e03ea37efa83d2e
Fábio <videiraft@gmail.com>
2019-08-10 17:46:58 +0200

    chore: initial setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+
+// Utility to ignore unnecessary files
+// when generating the glob patterns array for gulp.src()
+function addDefSrcIgnore(srcArr) {
+  return srcArr.concat([
+    '!**/REMOVE{,/**}',
+    '!node_modules{,/**}',
+    '!private{,/**}',
+    '!dist{,/**}',
+    '!.git{,/**}',
+    '!**/.DS_Store',
+  ]);
+}
+
+// JavaScript and JSON linter
+function lintJs() {
+  return gulp.src(addDefSrcIgnore(['**/*.js', '**/*.json']), { dot: true })
+    .pipe($.eslint({ dotfiles: true }))
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
+}
+
+exports.lint = gulp.parallel(
+  lintJs,
+);
